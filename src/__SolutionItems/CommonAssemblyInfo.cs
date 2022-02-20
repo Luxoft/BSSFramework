c27923f68822087668c09bbb2cb2403433dcffa8
Nikita <NikitaEgorov@users.noreply.github.com>
2022-02-20 22:45:08 +0300

    #34 Add Nullable reference types support to base bll methods (#36)
    * #34 Add Nullable reference types support to base bll methods
    
    * Fix build

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2022")]
 
-[assembly: AssemblyVersion("15.2.2.0")]
-[assembly: AssemblyFileVersion("15.2.2.0")]
-[assembly: AssemblyInformationalVersion("15.2.2")]
+[assembly: AssemblyVersion("16.0.0.0")]
+[assembly: AssemblyFileVersion("16.0.0.0")]
+[assembly: AssemblyInformationalVersion("16.0.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

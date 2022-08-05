8987599d6c868129cf42efbe269ad65153945343
iatsuta <98311820+iatsuta@users.noreply.github.com>
2022-08-05 23:50:26 +0200

    Update denormalize-auth.sql (#67)

--- src.other/AuthScripts/denormalize-auth.sql
+++ src.other/AuthScripts/denormalize-auth.sql
@@ -1,4 +1,10 @@
---delete FROM [auth].[DenormalizedPermissionItem] 
+--RunAlways = false
+--FileVersion = 1.0
+--Scheme = TRMSys
+--ApplyMode = PostAddOrUpdate
+
+use [$Database]
+go
 
 begin tran
 ------------------------------------------------------------------------------------------
@@ -12,26 +18,16 @@ INSERT INTO [appAudit].[AuditRevisionEntity]
            (@author,
 		    @now)
 
-			
 declare @newRevision bigint = @@IDENTITY
+------------------------------------------------------------------------------------------
+update pfi
+set contextEntityId = pfe.entityId,
+    entityTypeId = pfe.entityTypeId
+from [auth].[PermissionFilterItem] pfi
+join auth.PermissionFilterEntity pfe on pfe.id = pfi.entityId
 
 ------------------------------------------------------------------------------------------
-insert into [auth].[DenormalizedPermissionItem] (id, permissionId, entityId, entityTypeId, active, createdBy, createDate)
-select newid(), p.id, pfe.entityId, pfe.entityTypeId, 1, @author, @now
-from auth.permission p
-join auth.PermissionFilterItem pfi on pfi.permissionId = p.id
-join auth.PermissionFilterEntity pfe on pfe.id = pfi.entityId
-------------------------------------------------------------------------------------------
-insert into [auth].[DenormalizedPermissionItem] (id, permissionId, entityId, entityTypeId, active, createdBy, createDate)
-select newid(), p.id, '77777777-7777-7777-7777-777777777777', et.id, 1, @author, @now
-from auth.EntityType et
-join auth.permission p on 1 = 1
-where et.id not in (select pfe.entityTypeId
-                    from auth.PermissionFilterItem pfi
-					join auth.PermissionFilterEntity pfe on pfe.id = pfi.entityId
-					where pfi.permissionId = p.id)
-------------------------------------------------------------------------------------------
-INSERT INTO [authAudit].[DenormalizedPermissionItemAudit]
+INSERT INTO [authAudit].[PermissionFilterItemAudit]
            ([active]
            ,[Active_MOD]
            ,[createDate]
@@ -42,6 +38,8 @@ INSERT INTO [authAudit].[DenormalizedPermissionItemAudit]
            ,[ModifiedBy_MOD]
            ,[modifyDate]
            ,[ModifyDate_MOD]
+           ,[entityId]
+           ,[Entity_MOD]
            ,[entityTypeId]
            ,[EntityType_MOD]
            ,[permissionId]
@@ -49,28 +47,30 @@ INSERT INTO [authAudit].[DenormalizedPermissionItemAudit]
            ,[id]
            ,[REV]
            ,[REVTYPE]
-           ,[entityId]
-           ,[EntityId_MOD])
-     select active,
-            1,
-            createDate,
-            1,
-            createdBy,
-            1,
-            modifiedBy,
-            1,
-            modifyDate,
-            1,
-            entityTypeId,
-            1,
-            permissionId,
-            1,
-            id,
-            @newRevision,
-            0,
-            entityId,
-            1
-	from auth.DenormalizedPermissionItem
+           ,[contextEntityId]
+           ,[ContextEntityId_MOD])
+  select active
+           ,0
+           ,createDate
+           ,0
+           ,createdBy
+           ,0
+           ,modifiedBy
+           ,0
+           ,modifyDate
+           ,0
+           ,entityId
+           ,0
+           ,entityTypeId
+           ,1
+           ,permissionId
+           ,0
+           ,id
+           ,@newRevision
+           ,1
+           ,contextEntityId
+           ,1
+  from auth.PermissionFilterItem
 GO
 ------------------------------------------------------------------------------------------
 commit tran

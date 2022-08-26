7c1b5b3295a7a4f416c225d9753c74750bc6c7ed
iatstua <atsuta.ivan@dxc.com>
2022-08-26 23:29:32 +0200

    upd ver

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2022")]
 
-[assembly: AssemblyVersion("17.4.1.0")]
-[assembly: AssemblyFileVersion("17.4.1.0")]
-[assembly: AssemblyInformationalVersion("17.4.1")]
+[assembly: AssemblyVersion("17.4.2.0")]
+[assembly: AssemblyFileVersion("17.4.2.0")]
+[assembly: AssemblyInformationalVersion("17.4.2")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

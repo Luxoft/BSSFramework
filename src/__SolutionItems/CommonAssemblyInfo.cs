0fb0675638f460f25a87cd899e8e0fea53eac42c
iatsuta <98311820+iatsuta@users.noreply.github.com>
2022-11-15 16:54:58 +0100

    fix OverrideInput in AndSecurityPath (#99)
    Co-authored-by: iatstua <atsuta.ivan@dxc.com>

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2022")]
 
-[assembly: AssemblyVersion("17.6.1.0")]
-[assembly: AssemblyFileVersion("17.6.1.0")]
-[assembly: AssemblyInformationalVersion("17.6.1")]
+[assembly: AssemblyVersion("17.6.2.0")]
+[assembly: AssemblyFileVersion("17.6.2.0")]
+[assembly: AssemblyInformationalVersion("17.6.2")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

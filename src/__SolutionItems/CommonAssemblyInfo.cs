83df7831a5606c5a1b181817c86309dbe5d82975
IAtsuta <IAtsuta@IATSUTA-NB>
2023-08-18 13:41:45 +0200

    inc ver

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2023")]
 
-[assembly: AssemblyVersion("18.9.2.0")]
-[assembly: AssemblyFileVersion("18.9.2.0")]
-[assembly: AssemblyInformationalVersion("18.9.2")]
+[assembly: AssemblyVersion("19.0.0.0")]
+[assembly: AssemblyFileVersion("19.0.0.0")]
+[assembly: AssemblyInformationalVersion("19.0.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

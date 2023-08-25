f715c52559857f04685e783c7a798c12dabcc855
IAtsuta <IAtsuta@IATSUTA-NB>
2023-08-25 12:24:56 +0200

    separate-NHibernate

---
+++ src/_DomainDriven/Framework.DomainDriven.NHibernate.LegacyContext/NHibernateSetup/DependencyInjectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Framework.DependencyInjection;
+using Framework.DomainDriven.Tracking;
+using Framework.DomainDriven.DALExceptions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Framework.DomainDriven.NHibernate;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddLegacyDatabaseSettings(this IServiceCollection services)
+    {
+        services.AddScoped<IObjectStateService, NHibObjectStatesService>();
+
+        return services.ReplaceSingleton<IDalValidationIdentitySource, LegacyDalValidationIdentitySource>();
+    }
+}
--- src/_SampleSystem/SampleSystem.ServiceEnvironment/DependencyInjection/SampleSystemFrameworkDatabaseExtensions.cs
+++ src/_SampleSystem/SampleSystem.ServiceEnvironment/DependencyInjection/SampleSystemFrameworkDatabaseExtensions.cs
@@ -27,6 +27,7 @@ public static class SampleSystemFrameworkDatabaseExtensions
         var connectionString = configuration.GetConnectionString("DefaultConnection");
 
         return services.AddDatabaseSettings(connectionString)
+                       .AddLegacyDatabaseSettings()
                        .RegistryGenericDatabaseVisitors()
                        .RegistryDatabaseVisitors()
                        .RegisterSpecificationEvaluator();

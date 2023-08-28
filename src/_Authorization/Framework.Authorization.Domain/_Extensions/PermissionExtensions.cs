7dbf84b5e1d957f6dbd62a95430c21bc240259d5
IAtsuta <IAtsuta@IATSUTA-NB>
2023-08-28 16:25:08 +0200

    clean refs

--- src/_Authorization/Framework.Authorization.Domain/_Extensions/PermissionExtensions.cs
+++ src/_Authorization/Framework.Authorization.Domain/_Extensions/PermissionExtensions.cs
@@ -1,9 +1,4 @@
-﻿using Framework.Projection;
-using Framework.SecuritySystem;
-
-using DPermission = System.Collections.Generic.Dictionary<System.Type, System.Collections.Generic.List<System.Guid>>;
-
-namespace Framework.Authorization.Domain;
+﻿namespace Framework.Authorization.Domain;
 
 public static class PermissionExtensions
 {
@@ -12,18 +7,5 @@ public static class PermissionExtensions
         if (permission == null) throw new ArgumentNullException(nameof(permission));
 
         return permission.FilterItems.Select(fi => fi.Entity.EntityId).OrderBy(id => id);
-    }
-
-    public static DPermission ToDictionary(this IPermission<Guid> permission, IEnumerable<Type> securityTypes)
-    {
-        if (permission == null) throw new ArgumentNullException(nameof(permission));
-
-        var request = from filterItem in permission.FilterItems
-
-                      join securityType in securityTypes on filterItem.Entity.EntityType.Name equals securityType.GetProjectionSourceTypeOrSelf().Name
-
-                      group filterItem.Entity.EntityId by securityType;
-
-        return request.ToDictionary(g => g.Key, g => g.ToList());
     }
 }

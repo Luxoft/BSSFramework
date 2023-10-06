20cffef6b9d33d0a4d56e663229ffca0dfa251d2
IAtsuta <IAtsuta@IATSUTA-NB>
2023-10-06 20:48:25 +0200

    upd

---
+++ src/_Authorization/Framework.Authorization.SecuritySystem/ExternalSource/PlainAuthorizationTypedExternalSource.cs
@@ -0,0 +1,37 @@
+﻿using Framework.Authorization.Domain;
+using Framework.DomainDriven.Repository;
+using Framework.Persistent;
+using Framework.SecuritySystem;
+
+namespace Framework.Authorization.SecuritySystem.ExternalSource;
+
+public class PlainAuthorizationTypedExternalSource<TSecurityContext> : AuthorizationTypedExternalSourceBase<TSecurityContext>
+    where TSecurityContext : class, IIdentityObject<Guid>, IActiveObject, ISecurityContext
+{
+    private readonly ISecurityContextDisplayService<TSecurityContext> displayService;
+
+    public PlainAuthorizationTypedExternalSource(
+        IRepository<TSecurityContext> securityContextRepository,
+        IRepository<EntityType> entityTypeRepository,
+        IRepository<PermissionFilterEntity> permissionFilterEntityRepository,
+        SecurityContextInfo securityContextInfo,
+        ISecurityContextDisplayService<TSecurityContext> displayService)
+        : base(securityContextRepository, entityTypeRepository, permissionFilterEntityRepository, securityContextInfo)
+    {
+        this.displayService = displayService;
+    }
+
+    protected override SecurityEntity CreateSecurityEntity(TSecurityContext securityContext) =>
+
+        new SecurityEntity
+        {
+            Active = securityContext.Active,
+            Name = this.displayService.ToString(securityContext),
+            Id = securityContext.Id
+        };
+
+    protected override IEnumerable<TSecurityContext> GetSecurityEntitiesWithMasterExpand(TSecurityContext startSecurityObject)
+    {
+        return new[] { startSecurityObject };
+    }
+}

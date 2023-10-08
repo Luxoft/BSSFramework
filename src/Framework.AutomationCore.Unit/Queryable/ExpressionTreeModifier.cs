2340d2884520bfb3af68547b0efea91d517d3c01
Leshchev Artem <usethepowerluke@gmail.com>
2023-10-08 11:17:48 +0200

    Add helpers for unit tests (#237)

---
+++ src/Framework.AutomationCore.Unit/Queryable/ExpressionTreeModifier.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using NHibernate.Linq;
+
+namespace Framework.AutomationCore.Unit.Queryable;
+
+internal class ExpressionTreeModifier : ExpressionVisitor
+{
+    private static readonly HashSet<MethodInfo> VisitedMethods =
+        new[]
+            {
+                nameof(EagerFetchingExtensionMethods.Fetch),
+                nameof(EagerFetchingExtensionMethods.FetchMany),
+                nameof(EagerFetchingExtensionMethods.ThenFetch),
+                nameof(EagerFetchingExtensionMethods.ThenFetchMany)
+            }
+            .Select(x => typeof(EagerFetchingExtensionMethods).GetMethod(x)!)
+            .ToHashSet();
+
+    protected override Expression VisitMethodCall(MethodCallExpression node)
+    {
+        if (!IsFetchMethod(node.Method)) return base.VisitMethodCall(node);
+
+        var fetchInput = this.AvoidFetch(node);
+        return fetchInput.NodeType switch
+        {
+            ExpressionType.Constant => this.VisitConstant((ConstantExpression)fetchInput),
+            _ => fetchInput
+        };
+    }
+
+    private Expression AvoidFetch(MethodCallExpression node) =>
+        IsFetchMethod(node.Method) ? this.AvoidFetch((node.Arguments[0] as MethodCallExpression)!) : node;
+
+    private static bool IsFetchMethod(MethodInfo info) => VisitedMethods.Contains(info.GetGenericMethodDefinition());
+}
--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2023")]
 
-[assembly: AssemblyVersion("19.0.9.0")]
-[assembly: AssemblyFileVersion("19.0.9.0")]
-[assembly: AssemblyInformationalVersion("19.0.9")]
+[assembly: AssemblyVersion("19.0.10.0")]
+[assembly: AssemblyFileVersion("19.0.10.0")]
+[assembly: AssemblyInformationalVersion("19.0.10")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

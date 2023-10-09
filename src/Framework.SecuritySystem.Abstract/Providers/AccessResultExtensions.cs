566c35dff7a9e5eff1f9b17018b6b6a1bc6896df
IAtsuta <IAtsuta@IATSUTA-NB>
2023-10-09 15:25:13 +0200

    fix-DependencySecurityProvider

---
+++ src/Framework.SecuritySystem.Abstract/Providers/AccessResultExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Framework.SecuritySystem;
+
+public static class AccessResultExtensions
+{
+    public static AccessResult TryOverrideDomainObject<TDomainObject>(this AccessResult accessResult, TDomainObject domainObject)
+    {
+        switch (accessResult)
+        {
+            case AccessResult.AccessDeniedResult accessDeniedResult:
+                return accessDeniedResult with { DomainObjectInfo = (domainObject, typeof(TDomainObject)) };
+
+            default:
+                return accessResult;
+        }
+    }
+}

d16bb84253f2f4b6b18914b326ac6a83e2275d1b
Leshchev Artem <usethepowerluke@gmail.com>
2023-10-18 14:09:00 +0200

    Add possibility to endless retry RabbitMQ connection (#253)
    Co-authored-by: Artem Leshchev <artem.leshchev@dxc.com>

--- src/Framework.RabbitMq/Services/RabbitMqClient.cs
+++ src/Framework.RabbitMq/Services/RabbitMqClient.cs
@@ -15,7 +15,7 @@ public record RabbitMqClient(IOptions<RabbitMqServerSettings> Options, ILogger<R
 {
     private const int RetryConnectDelay = 5000;
 
-    public async Task<IConnection?> TryConnectAsync(int attempts, CancellationToken token = default)
+    public async Task<IConnection?> TryConnectAsync(int? attempts, CancellationToken token = default)
     {
         var serverSettings = this.Options.Value;
         var factory = new ConnectionFactory
@@ -40,12 +40,20 @@ public record RabbitMqClient(IOptions<RabbitMqServerSettings> Options, ILogger<R
         }
     }
 
-    private AsyncRetryPolicy CreateRetryPolicy(int attempts) =>
-        Policy.Handle<Exception>()
-              .WaitAndRetryAsync(
-                  attempts,
-                  _ => TimeSpan.FromMilliseconds(RetryConnectDelay),
-                  (ex, _) => this.LogConnectionError(ex));
+    private AsyncRetryPolicy CreateRetryPolicy(int? attempts)
+    {
+        var builder = Policy.Handle<Exception>();
+        if (attempts is null)
+            return builder.WaitAndRetryForeverAsync(
+                _ => TimeSpan.FromMilliseconds(RetryConnectDelay),
+                (ex, _) => this.LogConnectionError(ex));
+
+        return builder
+            .WaitAndRetryAsync(
+                attempts.Value,
+                _ => TimeSpan.FromMilliseconds(RetryConnectDelay),
+                (ex, _) => this.LogConnectionError(ex));
+    }
 
     private void LogConnectionError(Exception exception) => this.Logger.LogError(exception, "Could not connect to RabbitMQ server");
 }

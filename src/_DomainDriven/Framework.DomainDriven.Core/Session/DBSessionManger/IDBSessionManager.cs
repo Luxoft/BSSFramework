44c84caf48342da76b37633cc3963009bb033f44
iatsuta <98311820+iatsuta@users.noreply.github.com>
2023-10-19 10:50:19 +0200

    add DBSessionManager (#255)
    * add DBSessionManager
    
    ---------
    
    Co-authored-by: IAtsuta <IAtsuta@IATSUTA-NB>

---
+++ src/_DomainDriven/Framework.DomainDriven.Core/Session/DBSessionManger/IDBSessionManager.cs
@@ -0,0 +1,17 @@
+﻿namespace Framework.DomainDriven
+{
+    public interface IDBSessionManager
+    {
+        /// <summary>
+        /// Попытка закрыть текущую бд-сессию (если она существует) и вызывать механизм сброса евентов в DALListener-ы
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task TryCloseDbSessionAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        ///  Попытка промаркировать текущую бд-сессию (если она существует), что случилась ошибка и ничего в базу записывать не нужно.
+        /// </summary>
+        void TryFaultDbSession();
+    }
+}

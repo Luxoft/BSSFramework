46eb4e559e0f09d156a1aff129345afa3487144d
iatsuta <98311820+iatsuta@users.noreply.github.com>
2023-11-01 18:17:09 +0100

    Update CommonAssemblyInfo.cs

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2023")]
 
-[assembly: AssemblyVersion("19.1.11.0")]
-[assembly: AssemblyFileVersion("19.1.11.0")]
-[assembly: AssemblyInformationalVersion("19.1.11.0")]
+[assembly: AssemblyVersion("19.1.12.0")]
+[assembly: AssemblyFileVersion("19.1.12.0")]
+[assembly: AssemblyInformationalVersion("19.1.12.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

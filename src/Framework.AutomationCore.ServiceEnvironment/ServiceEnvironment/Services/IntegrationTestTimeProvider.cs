cbc84d83a3a7cac28c8b563ad110bfde5a78ff29
IAtsuta <IAtsuta@IATSUTA-NB>
2024-01-15 13:16:35 +0100

    upd

--- src/Framework.AutomationCore.ServiceEnvironment/ServiceEnvironment/Services/IntegrationTestTimeProvider.cs
+++ src/Framework.AutomationCore.ServiceEnvironment/ServiceEnvironment/Services/IntegrationTestTimeProvider.cs
@@ -1,26 +1,18 @@
 ﻿namespace Automation.ServiceEnvironment.Services;
 
 /// <summary>
-/// Реализация интерфейса <see cref="TimeProvider"/> для интеграционных тестов
+/// Реализация класса <see cref="TimeProvider"/> для интеграционных тестов
 /// </summary>
 public class IntegrationTestTimeProvider : TimeProvider
 {
-    private Func<DateTimeOffset> getUtcNow;
+    private TimeSpan offset;
 
-    public IntegrationTestTimeProvider() => this.Reset();
+    public virtual void Reset() => this.offset = TimeSpan.Zero;
 
-    public void Reset() => this.getUtcNow = () => DateTimeOffset.UtcNow;
-
-    public override DateTimeOffset GetUtcNow() => this.getUtcNow();
+    public override DateTimeOffset GetUtcNow() => DateTimeOffset.UtcNow + this.offset;
 
     public virtual void SetCurrentDateTime(DateTime dateTime)
     {
-        var dateTimeDelta = dateTime.Kind switch
-        {
-            DateTimeKind.Utc => dateTime - DateTimeOffset.UtcNow,
-            _ => dateTime - DateTimeOffset.Now
-        };
-
-        this.getUtcNow = () => DateTimeOffset.UtcNow + dateTimeDelta;
+        this.offset = dateTime - DateTimeOffset.UtcNow;
     }
 }

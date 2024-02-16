46435c8aed2efdab87f47965aa243efbdebccb5c
Ivan Gubanov <igubanov@luxoft.com>
2024-02-16 16:53:28 +0200

    test to reproduce bug - saving two different object with the same id

--- src/_SampleSystem/_Tests/SampleSystem.IntegrationTests/TransactionFlushBeforeCommit.cs
+++ src/_SampleSystem/_Tests/SampleSystem.IntegrationTests/TransactionFlushBeforeCommit.cs
@@ -1,7 +1,9 @@
 ﻿using FluentAssertions;
 
 using Framework.DomainDriven;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using SampleSystem.Domain;
 using SampleSystem.IntegrationTests.__Support.TestData;
 
@@ -16,26 +18,61 @@ public class TransactionFlushBeforeCommit : TestBase
         // Arrange
         var object1 = new Information { Name = "object1", Email = "email@luxoft.fake" };
         this.Evaluate(
-                      DBSessionMode.Write,
-                      context => context.Logics.Information.Insert(object1, Guid.NewGuid()));
+            DBSessionMode.Write,
+            context => context.Logics.Information.Insert(object1, Guid.NewGuid()));
 
         var object2 = new Information { Name = "object2", Email = object1.Email };
 
         // Act
-        this.Evaluate(DBSessionMode.Write, context =>
-                                           {
-                                               context.Logics.Information.Insert(object2, Guid.NewGuid());
+        this.Evaluate(DBSessionMode.Write,
+                      context =>
+                      {
+                          context.Logics.Information.Insert(object2, Guid.NewGuid());
 
-                                               var oldObject = context.Logics.Information.GetUnsecureQueryable().Single(x => x.Email == object1.Email);
+                          var oldObject = context.Logics.Information.GetUnsecureQueryable().Single(x => x.Email == object1.Email);
 
-                                               context.Logics.Information.Remove(oldObject);
-                                           });
+                          context.Logics.Information.Remove(oldObject);
+                      });
 
         // Assert
         this.Evaluate(
-                      DBSessionMode.Read,
-                      context => context.Logics.Information.GetUnsecureQueryable().Single(x => x.Email == object1.Email))
+                DBSessionMode.Read,
+                context => context.Logics.Information.GetUnsecureQueryable().Single(x => x.Email == object1.Email))
             .Name
-            .Should().BeEquivalentTo(object2.Name);
+            .Should()
+            .BeEquivalentTo(object2.Name);
+    }
+
+    [TestMethod]
+    public void FewDifferentTypeObjectWithTheSameId_Should_SuccessfullySave()
+    {
+        // Arrange
+        var objectId1 = Guid.NewGuid();
+        var objectId2 = objectId1; // NOTE: with different guids test successfully completes
+        var object1 = new Information { Name = "object1", Email = "email@luxoft.fake" };
+        var object2 = new Example1 { Field1 = Guid.NewGuid() };
+
+        // Act
+        this.Evaluate(DBSessionMode.Write,
+                      context =>
+                      {
+                          context.Logics.Information.Insert(object1, objectId1);
+                          context.Logics.Example1.Insert(object2, objectId2);
+                      });
+
+        // Assert
+        this.Evaluate(
+                DBSessionMode.Read,
+                context => context.Logics.Information.GetUnsecureQueryable().Single(x => x.Id == objectId1))
+            .Name
+            .Should()
+            .BeEquivalentTo(object1.Name);
+
+        this.Evaluate(
+                DBSessionMode.Read,
+                context => context.Logics.Example1.GetUnsecureQueryable().Single(x => x.Id == objectId2))
+            .Field1
+            .Should()
+            .Be(object2.Field1);
     }
 }

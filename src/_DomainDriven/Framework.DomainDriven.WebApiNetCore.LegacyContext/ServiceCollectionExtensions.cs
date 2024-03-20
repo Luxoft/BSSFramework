a7c19f92859e3a7b2608324c443f345ea6340d7e
iatsuta <98311820+iatsuta@users.noreply.github.com>
2024-03-20 11:49:17 +0100

    fix RegisterContextEvaluators (#316)
    Co-authored-by: IAtsuta <IAtsuta@IATSUTA-NB>

--- src/_DomainDriven/Framework.DomainDriven.WebApiNetCore.LegacyContext/ServiceCollectionExtensions.cs
+++ src/_DomainDriven/Framework.DomainDriven.WebApiNetCore.LegacyContext/ServiceCollectionExtensions.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Framework.DomainDriven.ServiceModel;
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Framework.DomainDriven.WebApiNetCore;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection RegisterContextEvaluators(this IServiceCollection services, bool useSingleCall = true)
+    public static IServiceCollection RegisterContextEvaluators(this IServiceCollection services, bool useSingleCall = false)
     {
         services.AddScoped(
             typeof(IApiControllerBaseEvaluator<,>),
             useSingleCall ? typeof(ApiControllerBaseSingleCallEvaluator<,>) : typeof(ApiControllerNewScopeEvaluator<,>));
+
+        if (!useSingleCall)
+        {
+            services.AddSingleton(typeof(IContextEvaluator<,>), typeof(ContextEvaluator<,>));
+        }
 
         return services;
     }

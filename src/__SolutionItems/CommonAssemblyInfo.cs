93b9f120d5d8e6fea90e642a9d5ce033a1ddce8a
iatsuta <98311820+iatsuta@users.noreply.github.com>
2024-04-08 15:59:05 +0200

    clean-EventsSubscriptionManager (#320)
    Co-authored-by: IAtsuta <IAtsuta@IATSUTA-NB>

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2024")]
 
-[assembly: AssemblyVersion("20.1.6.0")]
-[assembly: AssemblyFileVersion("20.1.6.0")]
-[assembly: AssemblyInformationalVersion("20.1.6.0")]
+[assembly: AssemblyVersion("20.1.7.0")]
+[assembly: AssemblyFileVersion("20.1.7.0")]
+[assembly: AssemblyInformationalVersion("20.1.7.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

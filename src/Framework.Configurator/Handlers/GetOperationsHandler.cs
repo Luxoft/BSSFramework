c222109a00bd1ed7a9711b3f87c57ee4de6815b4
Artem Leshchev <artem.leshchev@dxc.com>
2024-04-11 21:15:22 +0200

    get operation list

--- src/Framework.Configurator/Handlers/GetOperationsHandler.cs
+++ src/Framework.Configurator/Handlers/GetOperationsHandler.cs
@@ -1,18 +1,23 @@
-﻿using Framework.Authorization.BLL;
-using Framework.Configurator.Interfaces;
+﻿using Framework.Configurator.Interfaces;
+using Framework.Configurator.Models;
+using Framework.SecuritySystem;
 
 using Microsoft.AspNetCore.Http;
 
 namespace Framework.Configurator.Handlers;
 
-public class GetOperationsHandler : BaseReadHandler, IGetOperationsHandler
-
+public class GetOperationsHandler(IOperationAccessor operationAccessor, ISecurityRoleSource roleSource)
+    : BaseReadHandler, IGetOperationsHandler
 {
-    private readonly IAuthorizationBLLContext authorizationBllContext;
-
-    public GetOperationsHandler(IAuthorizationBLLContext authorizationBllContext) =>
-            this.authorizationBllContext = authorizationBllContext;
-
     protected override object GetData(HttpContext context)
-        => new List<string>();
+    {
+        if (!operationAccessor.IsAdmin()) return new List<string>();
+
+        return roleSource.SecurityRoles
+                         .SelectMany(x => x.Operations)
+                         .Select(o => new OperationDto { Name = o.Name, Description = o.Description })
+                         .OrderBy(x => x.Name)
+                         .DistinctBy(x => x.Name)
+                         .ToList();
+    }
 }

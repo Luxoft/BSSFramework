03a89c970c6d50b92198aa6dc3ee377edb932df1
IAtsuta <IAtsuta@IATSUTA-NB>
2024-04-12 13:11:16 +0200

    simplify-integration-generation

--- src/_SampleSystem/SampleSystem.WebApiCore/Controllers/_Generated/Integration/TestImmutableObjService.Generated.cs
+++ src/_SampleSystem/SampleSystem.WebApiCore/Controllers/_Generated/Integration/TestImmutableObjService.Generated.cs
@@ -22,7 +22,7 @@
         protected virtual SampleSystem.Generated.DTO.TestImmutableObjIdentityDTO SaveTestImmutableObjInternal(SampleSystem.Generated.DTO.TestImmutableObjIntegrationRichDTO testImmutableObj, Framework.DomainDriven.ServiceModel.Service.EvaluatedData<SampleSystem.BLL.ISampleSystemBLLContext, SampleSystem.Generated.DTO.ISampleSystemDTOMappingService> evaluateData)
         {
             SampleSystem.BLL.ITestImmutableObjBLL bll = evaluateData.Context.Logics.TestImmutableObj;
-            evaluateData.Context.Authorization.AuthorizationSystem.CheckAccess(SampleSystem.Security.SampleSystemSecurityRole.SystemIntegration);
+            evaluateData.Context.Authorization.AuthorizationSystem.CheckAccess(Framework.SecuritySystem.SpecialRoleSecurityRule.SystemIntegration);
             return this.SaveTestImmutableObjInternal(testImmutableObj, evaluateData, bll);
         }
         

eecf1975df1dd373cbf9393184db9b40bf0fa398
IAtsuta <IAtsuta@IATSUTA-NB>
2024-04-12 12:07:48 +0200

    add comments

--- src/Framework.SecuritySystem.Abstract/SecurityRule/SpecialRoleSecurityRule.cs
+++ src/Framework.SecuritySystem.Abstract/SecurityRule/SpecialRoleSecurityRule.cs
@@ -1,9 +1,19 @@
 ﻿namespace Framework.SecuritySystem;
 
+/// <summary>
+/// Правила доступа для специальных ролей
+/// </summary>
+/// <param name="Name"></param>
 public record SpecialRoleSecurityRule(string Name) : SecurityRule.DomainObjectSecurityRule
 {
+    /// <summary>
+    /// Правило доступа по администраторской роли
+    /// </summary>
     public static SpecialRoleSecurityRule Administrator { get; } = new (nameof(Administrator));
 
+    /// <summary>
+    /// Правло доступа для интеграционной роли
+    /// </summary>
     public static SpecialRoleSecurityRule SystemIntegration { get; } = new(nameof(SystemIntegration));
 
     public override string ToString() => this.Name;

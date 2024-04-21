9479951c7a2bdc0fd621cd3eda4a5dc26d20cd2f
Artem Leshchev <artem.leshchev@dxc.com>
2024-04-21 19:07:10 +0200

    Migrate packages to own repo https://github.com/Luxoft/bss-platform

--- src/Framework.RabbitMq/Interfaces/IRabbitMqClient.cs
+++
@@ -1,8 +0,0 @@
-using RabbitMQ.Client;
-
-namespace Framework.RabbitMq.Interfaces;
-
-public interface IRabbitMqClient
-{
-    Task<IConnection?> TryConnectAsync(int? attempts, CancellationToken token = default);
-}

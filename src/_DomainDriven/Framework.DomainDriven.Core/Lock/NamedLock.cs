bf596ed129c4c874a6c973115534c24d376f1bef
Atsuta, Ivan <IAtsuta@luxoft.com>
2024-06-25 10:20:20 +0200

    simplify-NamedLock

--- src/_DomainDriven/Framework.DomainDriven.Core/Lock/NamedLock.cs
+++ src/_DomainDriven/Framework.DomainDriven.Core/Lock/NamedLock.cs
@@ -3,4 +3,10 @@
 /// <summary>
 /// Операция для объекта, на котором можно сделать пессимистическую блокировку
 /// </summary>
-public record NamedLock(string Name, Type DomainType);
+public record NamedLock(string Name, Type DomainType)
+{
+    public NamedLock(string Name)
+        : this(Name, typeof(object))
+    {
+    }
+}

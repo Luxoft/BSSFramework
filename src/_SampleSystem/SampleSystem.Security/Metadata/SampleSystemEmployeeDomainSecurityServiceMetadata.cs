2a053be4e671338ca5f2530c019b0fd659c068f7
iatsuta <98311820+iatsuta@users.noreply.github.com>
2024-07-03 21:28:43 +0200

    fix-generate-securityRule (#377)

--- src/_SampleSystem/SampleSystem.Security/Metadata/SampleSystemEmployeeDomainSecurityServiceMetadata.cs
+++ src/_SampleSystem/SampleSystem.Security/Metadata/SampleSystemEmployeeDomainSecurityServiceMetadata.cs
@@ -10,8 +10,7 @@ public class SampleSystemEmployeeDomainSecurityServiceMetadata
     : IDomainSecurityServiceMetadata<Employee>
 {
     public static void Setup(IDomainSecurityServiceBuilder<Employee> builder) =>
-        builder.SetView(
-                   SampleSystemSecurityOperation.EmployeeView.ToSecurityRule().Or<CurrentEmployeeSecurityProvider<Employee>>())
+        builder.SetView(SampleSystemSecurityOperation.EmployeeView.ToSecurityRule().Or<CurrentEmployeeSecurityProvider<Employee>>())
                .SetEdit(SampleSystemSecurityOperation.EmployeeEdit)
                .SetPath(
                    SecurityPath<Employee>.Create(employee => employee).And(employee => employee.CoreBusinessUnit)

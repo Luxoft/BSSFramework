6637cc41ea2758af72c90d159415f295874338bb
Atsuta, Ivan <IAtsuta@luxoft.com>
2024-07-30 14:27:01 +0200

    upd SubscriptionInitializer

--- src/_Configuration/Framework.Configuration.BLL.Core/SubscriptionSystemService/SubscriptionSystemService3/Subscriptions/SubscriptionInitializer.cs
+++ src/_Configuration/Framework.Configuration.BLL.Core/SubscriptionSystemService/SubscriptionSystemService3/Subscriptions/SubscriptionInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using Framework.Configuration.Domain;
 using Framework.DomainDriven.Repository;
+using Framework.DomainDriven.Tracking;
 using Framework.SecuritySystem;
 
 namespace Framework.Configuration.BLL.SubscriptionSystemService3.Subscriptions;
@@ -7,14 +8,20 @@ namespace Framework.Configuration.BLL.SubscriptionSystemService3.Subscriptions;
 public class SubscriptionInitializer(
     [DisabledSecurity] IRepository<CodeFirstSubscription> targetSystemTargetSystem,
     IConfigurationBLLContext context,
-    SubscriptionMetadataStore metadataStore) : ISubscriptionInitializer
+    SubscriptionMetadataStore metadataStore,
+    IPersistentInfoService persistentInfoService) : ISubscriptionInitializer
 {
     public async Task Initialize(CancellationToken cancellationToken)
     {
         var subscriptions = metadataStore
                             .store
                             .SelectMany(g => g)
-                            .Select(m => new CodeFirstSubscription(m.Code, context.GetDomainType(m.DomainObjectType, true)))
+                            .Select(m => new CodeFirstSubscription(
+                                        m.Code,
+                                        context.GetDomainType(
+                                            m.DomainObjectType,
+                                            persistentInfoService.IsPersistent(m.DomainObjectType))
+                                        ?? new DomainType(new TargetSystem(false, false, false))))
                             .ToArray();
 
         foreach (var subscription in subscriptions)

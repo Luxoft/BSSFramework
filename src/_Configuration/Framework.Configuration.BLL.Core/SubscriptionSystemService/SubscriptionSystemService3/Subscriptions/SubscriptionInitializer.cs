0c42a59baee0ec5a115f88fcdabe0267ba322faf
iatsuta <98311820+iatsuta@users.noreply.github.com>
2024-07-30 14:46:42 +0200

    upd SubscriptionInitializer (#408)
    * upd SubscriptionInitializer

--- src/_Configuration/Framework.Configuration.BLL.Core/SubscriptionSystemService/SubscriptionSystemService3/Subscriptions/SubscriptionInitializer.cs
+++ src/_Configuration/Framework.Configuration.BLL.Core/SubscriptionSystemService/SubscriptionSystemService3/Subscriptions/SubscriptionInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using Framework.Configuration.Domain;
 using Framework.DomainDriven.Repository;
+using Framework.DomainDriven.Tracking;
 using Framework.SecuritySystem;
 
 namespace Framework.Configuration.BLL.SubscriptionSystemService3.Subscriptions;
@@ -7,19 +8,34 @@ namespace Framework.Configuration.BLL.SubscriptionSystemService3.Subscriptions;
 public class SubscriptionInitializer(
     [DisabledSecurity] IRepository<CodeFirstSubscription> targetSystemTargetSystem,
     IConfigurationBLLContext context,
-    SubscriptionMetadataStore metadataStore) : ISubscriptionInitializer
+    SubscriptionMetadataStore metadataStore,
+    IPersistentInfoService persistentInfoService) : ISubscriptionInitializer
 {
     public async Task Initialize(CancellationToken cancellationToken)
     {
         var subscriptions = metadataStore
                             .store
                             .SelectMany(g => g)
-                            .Select(m => new CodeFirstSubscription(m.Code, context.GetDomainType(m.DomainObjectType, true)))
+                            .Select(
+                                m => new CodeFirstSubscription(
+                                    m.Code,
+                                    context.GetDomainType(
+                                        m.DomainObjectType,
+                                        persistentInfoService.IsPersistent(m.DomainObjectType))
+                                    ?? this.CreateRuntime(m.DomainObjectType)))
                             .ToArray();
 
         foreach (var subscription in subscriptions)
         {
             await targetSystemTargetSystem.SaveAsync(subscription, cancellationToken);
         }
+    }
+
+    private DomainType CreateRuntime(Type type)
+    {
+        return new DomainType(new TargetSystem(false, false, false) { Name = "Runtime TargetSystem" })
+               {
+                   Name = type.Name, NameSpace = type.Namespace
+               };
     }
 }

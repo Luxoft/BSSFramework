b6ccc869d772f3a58f75f314e2f21d53280169aa
Atsuta, Ivan <IAtsuta@luxoft.com>
2024-07-30 12:41:39 +0200

    upd DomainTypeInfo

--- src/_SampleSystem/SampleSystem.ServiceEnvironment/DependencyInjection/SampleSystemFrameworkExtensions.cs
+++ src/_SampleSystem/SampleSystem.ServiceEnvironment/DependencyInjection/SampleSystemFrameworkExtensions.cs
@@ -51,22 +51,8 @@ public static class SampleSystemFrameworkExtensions
                     true,
                     new DomainTypeInfo[]
                         {
-                            new(typeof(AnotherSqlParserTestObj), new Guid("{2328B905-DD6F-4304-A406-09A8D56A365F}")),
-                            new(typeof(BusinessUnit), new Guid("5C326B10-B4B4-402C-BCCE-A311016CB715")),
                             new(typeof(Country), new Guid("{C6030B2D-16F1-4854-9FAB-8A69B7FFAC6C}")),
-                            new(typeof(Location), new Guid("CACA9DB4-9DA6-48AA-9FD3-A311016CB715")),
                             new(typeof(Employee), new Guid("{AA46DA53-9B21-4DEC-9C70-720BDA1CB198}")),
-                            new(typeof(EmployeeCellPhone), new Guid("9D3EF98C-B857-40EF-A170-DB1285E4CE28")),
-                            new(typeof(TestRestrictionObject), new Guid("{1A7C78AD-9371-4DAF-895C-EF6E5A8A0350}")),
-                            new(typeof(HRDepartment), new Guid("0BE31997-C4CD-449E-9394-A311016CB715")),
-                            new(typeof(IntegrationVersionContainer1), new Guid("D1972415-C65B-42D7-ADBB-561B03935E70")),
-                            new(typeof(Principal), new Guid("DB66670A-6A1A-4F0E-BDAE-20ED291B2ACC")),
-                            new(typeof(Project), new Guid("{D79C7F5B-2968-4ACA-91FA-E12B36F121E2}")),
-                            new(typeof(SqlParserTestObj), new Guid("{4963D86E-5650-41E0-BDBA-0274FF2CF375}")),
-                            new(typeof(SqlParserTestObjContainer), new Guid("{6502514C-2B88-40BF-8D01-C3DFAB008599}")),
-                            new(typeof(ManagementUnitFluentMapping), new Guid("11E78AEF-9512-46E0-A33D-AAE58DC7E18C")),
-                            new(typeof(IntegrationVersionContainer2), new Guid("4FAB2A8D-600F-42E4-A72C-5B675BD60BAC")),
-                            new(typeof(ManagementUnit), new Guid("77E78AEF-9512-46E0-A33D-AAE58DC7E18C")),
                         }));
 
     public static IServiceCollection RegisterSupportLegacyServices(this IServiceCollection services)

3405d7663b312f77cd43a9f29733b69753c80ff1
iatsuta <98311820+iatsuta@users.noreply.github.com>
2024-08-01 10:37:54 +0200

    upd SecurityAccessorData (#413)

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2024")]
 
-[assembly: AssemblyVersion("21.5.6.0")]
-[assembly: AssemblyFileVersion("21.5.6.0")]
-[assembly: AssemblyInformationalVersion("21.5.6.0")]
+[assembly: AssemblyVersion("21.5.7.0")]
+[assembly: AssemblyFileVersion("21.5.7.0")]
+[assembly: AssemblyInformationalVersion("21.5.7.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

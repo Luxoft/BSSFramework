b5b2fe78a491c909addf00a7f4851ade8bfde9ba
Atsuta, Ivan <IAtsuta@luxoft.com>
2024-08-05 19:15:27 +0200

    upd

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -1,15 +1,21 @@
 ﻿using System.Reflection;
 
+
 [assembly: AssemblyProduct("BSS Framework")]
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2024")]
 
-[assembly: AssemblyVersion("21.5.7.0")]
-[assembly: AssemblyFileVersion("21.5.7.0")]
-[assembly: AssemblyInformationalVersion("21.5.7.0")]
+[assembly: AssemblyVersion(AssemblyVersionData.Value)]
+[assembly: AssemblyFileVersion(AssemblyVersionData.Value)]
+[assembly: AssemblyInformationalVersion(AssemblyVersionData.Value)]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
 [assembly: AssemblyConfiguration("Release")]
 #endif
+
+internal static class AssemblyVersionData
+{
+    public const string Value = "21.5.8.0";
+}

7329569d62a6858f8d0d5c2d8d3de64bbe589aca
iatsuta <98311820+iatsuta@users.noreply.github.com>
2024-08-27 21:41:15 +0200

    clean authorization system 2 (#438)
    * clean AuthorizationSystem 2

---
+++ src/_Authorization/Framework.Authorization.Environment/Security/CurrentPrincipalSecurityProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+using Framework.Authorization.Domain;
+using Framework.Authorization.SecuritySystem;
+using Framework.Core;
+using Framework.SecuritySystem;
+
+namespace Framework.Authorization.Environment.Security;
+
+public class CurrentPrincipalSecurityProvider<TDomainObject>(
+    ICurrentPrincipalSource currentPrincipalSource,
+    IRelativeDomainPathInfo<TDomainObject, Principal> toPrincipalPathInfo)
+    : SecurityProvider<TDomainObject>
+{
+    public override Expression<Func<TDomainObject, bool>> SecurityFilter { get; } =
+
+        ExpressionHelper.Create((Principal principal) => principal == currentPrincipalSource.CurrentPrincipal)
+                        .OverrideInput(toPrincipalPathInfo.Path);
+
+    protected override LambdaCompileMode SecurityFilterCompileMode { get; } = LambdaCompileMode.All;
+
+    public override SecurityAccessorData GetAccessorData(TDomainObject domainObject) =>
+        SecurityAccessorData.Return(currentPrincipalSource.CurrentPrincipal.Name);
+}

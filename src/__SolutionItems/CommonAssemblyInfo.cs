df467ddc0e27c073e2f14548a40b37a726ad49e2
Atsuta, Ivan <IAtsuta@luxoft.com>
2024-10-01 10:09:58 +0200

    upd

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2024")]
 
-[assembly: AssemblyVersion("22.3.13.0")]
-[assembly: AssemblyFileVersion("22.3.13.0")]
-[assembly: AssemblyInformationalVersion("22.3.13.0")]
+[assembly: AssemblyVersion("22.3.14.0")]
+[assembly: AssemblyFileVersion("22.3.14.0")]
+[assembly: AssemblyInformationalVersion("22.3.14.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

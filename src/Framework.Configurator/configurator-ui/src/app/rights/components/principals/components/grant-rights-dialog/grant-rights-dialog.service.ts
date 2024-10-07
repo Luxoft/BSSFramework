84796adb98afc926a6c2c8ac2b96ee4ca47a2b64
Ivan Gubanov <igubanov@luxoft.com>
2024-10-07 16:56:37 +0300

    remove/edit by index, not empty id

--- src/Framework.Configurator/configurator-ui/src/app/rights/components/principals/components/grant-rights-dialog/grant-rights-dialog.service.ts
+++ src/Framework.Configurator/configurator-ui/src/app/rights/components/principals/components/grant-rights-dialog/grant-rights-dialog.service.ts
@@ -51,7 +51,7 @@ export class GrantRightsDialogService {
       });
   }
 
-  public remove(permission: IPermissionDto): void {
+  public remove(elementIndex: number): void {
     this.dialog
       .open(ConfirmDialogComponent, {
         data: { title: 'Are you sure you want to delete this role?', button: 'Yes, delete' },
@@ -65,7 +65,7 @@ export class GrantRightsDialogService {
             return;
           }
           const rights = this.rightsInternalSubject.value;
-          rights.Permissions = rights.Permissions.filter((x) => x.Id != permission.Id);
+          rights.Permissions.splice(elementIndex, 1);
           this.rightsInternalSubject.next(rights);
         }),
         takeUntil(this.destroy$)
@@ -73,36 +73,11 @@ export class GrantRightsDialogService {
       .subscribe();
   }
 
-  public edit(permissionDto: IPermissionDto, units: IRoleContext[]): void {
-    const permission = this.mapPermission(permissionDto, this.rolesSubject.value, this.allContextsSubject.value);
+  public edit(permissionDto: IPermissionDto, elementIndex: number): void {
+    const allContexts = this.allContextsSubject.value;
+    const permission = this.mapPermission(permissionDto, this.rolesSubject.value, allContexts);
 
-    this.dialog
-      .open(PermissionEditDialogComponent, {
-        maxHeight: '90vh',
-        maxWidth: '90vw',
-        data: { permission, units },
-      })
-      .afterClosed()
-      .pipe(
-        tap((result) => {
-          if (result) {
-            const rights = this.rightsInternalSubject.value;
-            const findIndex = rights.Permissions.findIndex((x) => x.Id === result.Id);
-            if (findIndex > -1) {
-              rights.Permissions[findIndex] = result;
-            } else {
-              rights.Permissions.unshift(result);
-            }
-            this.rightsInternalSubject.next(rights);
-          }
-          // TODO: fix next three lines
-          const filterValue = this.filter.value;
-          this.filter.next({ ...filterValue, comment: '' });
-          this.filter.next({ ...filterValue });
-        }),
-        takeUntil(this.destroy$)
-      )
-      .subscribe();
+    this.openPermissionEditDialog(permission, (permissions, permission) => (permissions[elementIndex] = permission));
   }
 
   public addPermission(): void {
@@ -123,7 +98,38 @@ export class GrantRightsDialogService {
             Comment: '',
             Contexts: [],
           };
-          this.edit(permission, this.allContextsSubject.value);
+          this.openPermissionEditDialog(permission, (permissions, permission) => permissions.push(permission));
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  private openPermissionEditDialog<TPermission extends IPermissionDto>(
+    permissionDto: IPermissionDto,
+    updPermissionsAction: (existingPermissions: IPermissionDto[], newPermission: TPermission) => void
+  ): void {
+    const allContexts = this.allContextsSubject.value;
+    const permission = this.mapPermission(permissionDto, this.rolesSubject.value, allContexts);
+
+    this.dialog
+      .open(PermissionEditDialogComponent, {
+        maxHeight: '90vh',
+        maxWidth: '90vw',
+        data: { permission, allContexts },
+      })
+      .afterClosed()
+      .pipe(
+        tap((result) => {
+          if (result) {
+            const rights = this.rightsInternalSubject.value;
+            updPermissionsAction(rights.Permissions, result);
+            this.rightsInternalSubject.next(rights);
+          }
+          // TODO: fix next three lines
+          const filterValue = this.filter.value;
+          this.filter.next({ ...filterValue, comment: '' });
+          this.filter.next({ ...filterValue });
         }),
         takeUntil(this.destroy$)
       )

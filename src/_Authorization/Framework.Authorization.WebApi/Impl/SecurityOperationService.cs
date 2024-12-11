3b29b6ee1f6a4048c8b5f178738db1b7a48b99f8
iatsuta <98311820+iatsuta@users.noreply.github.com>
2024-12-11 12:10:15 +0100

    fix SelectOperationResult (#526)

--- src/_Authorization/Framework.Authorization.WebApi/Impl/SecurityOperationService.cs
+++ src/_Authorization/Framework.Authorization.WebApi/Impl/SecurityOperationService.cs
@@ -1,14 +1,17 @@
 ﻿using Framework.SecuritySystem.AvailableSecurity;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Framework.Authorization.WebApi;
 
-public partial class AuthSLJsonController(IAvailableSecurityOperationSource availableSecurityOperationSource)
+public partial class AuthSLJsonController
 {
     [HttpPost]
     public virtual async Task<IEnumerable<string>> GetSecurityOperations(CancellationToken cancellationToken = default)
     {
+        var availableSecurityOperationSource = this.HttpContext.RequestServices.GetRequiredService<IAvailableSecurityOperationSource>();
+
         var operations = await availableSecurityOperationSource.GetAvailableSecurityOperations(cancellationToken);
 
         return operations.Select(op => op.Name);

59482f6ee2c0259cf4ede1c1eb565f03fb2d8bef
Dmitrii Markovkin <62047582+dmarkovkin@users.noreply.github.com>
2025-03-26 15:28:15 +0100

    Update Luxoft.Bss.Testing.Xunit package (#552)
    Co-authored-by: DMarkovkin <dmarkovkin@luxoft.com>

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2025")]
 
-[assembly: AssemblyVersion("23.2.3.0")]
-[assembly: AssemblyFileVersion("23.2.3.0")]
-[assembly: AssemblyInformationalVersion("23.2.3.0")]
+[assembly: AssemblyVersion("23.2.4.0")]
+[assembly: AssemblyFileVersion("23.2.4.0")]
+[assembly: AssemblyInformationalVersion("23.2.4.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

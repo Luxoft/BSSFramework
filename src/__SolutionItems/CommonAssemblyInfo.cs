f0d940f1f2f8648e89266fbd0e10b207ef3943c6
iatsuta <98311820+iatsuta@users.noreply.github.com>
2025-04-17 13:01:23 +0200

    upd-github-workflows (#558)
    * upd-github-workflows

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2025")]
 
-[assembly: AssemblyVersion("23.2.9.0")]
-[assembly: AssemblyFileVersion("23.2.9.0")]
-[assembly: AssemblyInformationalVersion("23.2.9.0")]
+[assembly: AssemblyVersion("23.2.9.2")]
+[assembly: AssemblyFileVersion("23.2.9.2")]
+[assembly: AssemblyInformationalVersion("23.2.9.2")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

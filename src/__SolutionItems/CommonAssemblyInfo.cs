ca703acb63ab46aba3beb465a685fda45d05086e
Atsuta, Ivan <IAtsuta@luxoft.com>
2025-04-17 13:15:31 +0200

    upd-github-workflows-2

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2025")]
 
-[assembly: AssemblyVersion("23.2.9.2")]
-[assembly: AssemblyFileVersion("23.2.9.2")]
-[assembly: AssemblyInformationalVersion("23.2.9.2")]
+[assembly: AssemblyVersion("23.2.9.3")]
+[assembly: AssemblyFileVersion("23.2.9.3")]
+[assembly: AssemblyInformationalVersion("23.2.9.3")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

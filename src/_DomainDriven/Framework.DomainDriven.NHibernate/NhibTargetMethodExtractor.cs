ca967e831bdec28639dda3de49e2ca1aee81d7e8
Atsuta, Ivan <IAtsuta@luxoft.com>
2025-11-24 13:05:06 +0100

    upd GenericQueryable

--- src/_DomainDriven/Framework.DomainDriven.NHibernate/NhibTargetMethodExtractor.cs
+++ src/_DomainDriven/Framework.DomainDriven.NHibernate/NhibTargetMethodExtractor.cs
@@ -8,6 +8,15 @@ public class NhibTargetMethodExtractor() : TargetMethodExtractor([typeof(LinqExt
 {
     private static class NhibLinqExtensions
     {
+        public static async Task<TSource[]> ToArrayAsync<TSource>(
+            IQueryable<TSource> source,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var elements = await source.ToFuture().GetEnumerableAsync(cancellationToken);
+
+            return elements.ToArray();
+        }
+
         public static async Task<HashSet<TSource>> ToHashSetAsync<TSource>(
             IQueryable<TSource> source,
             IEqualityComparer<TSource>? comparer,

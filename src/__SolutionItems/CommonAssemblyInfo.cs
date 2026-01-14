8bd66a028a56178d344240609dae0f278a763436
Atsuta, Ivan <IAtsuta@luxoft.com>
2026-01-14 11:14:59 +0100

    upd libs

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2026")]
 
-[assembly: AssemblyVersion("25.1.6.0")]
-[assembly: AssemblyFileVersion("25.1.6.0")]
-[assembly: AssemblyInformationalVersion("25.1.6.0")]
+[assembly: AssemblyVersion("25.1.7.0")]
+[assembly: AssemblyFileVersion("25.1.7.0")]
+[assembly: AssemblyInformationalVersion("25.1.7.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

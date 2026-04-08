823c485dad7d5641c0ac8620da0b5c7020c294e7
Atsuta, Ivan <IAtsuta@luxoft.com>
2026-04-08 02:47:27 +0200

    upd

--- src/Database/Framework.Database.EntityFramework/DependencyInjection/BssFrameworkSettingsExtensions.cs
+++ src/Database/Framework.Database.EntityFramework/DependencyInjection/BssFrameworkSettingsExtensions.cs
@@ -4,7 +4,8 @@ namespace Framework.Database.EntityFramework.DependencyInjection;
 
 public static class BssFrameworkBuilderSetupExtensions
 {
-    public static TSelf AddEntityFramework<TSelf>(this IBssFrameworkSetup<TSelf> setup, Action<IEntityFrameworkSetup>? setupAction = null) =>
+    public static TSelf AddEntityFramework<TSelf>(this IBssFrameworkSetup<TSelf> setup, Action<IEntityFrameworkSetup>? setupAction = null)
+        where TSelf : IBssFrameworkSetup<TSelf> =>
 
         setup.AddExtensions(new BssFrameworkExtension(services => services.AddEntityFramework(setupAction)));
 }
--- src/Database/Framework.Database.NHibernate/DependencyInjection/BssFrameworkSetupExtensions.cs
+++ src/Database/Framework.Database.NHibernate/DependencyInjection/BssFrameworkSetupExtensions.cs
@@ -6,7 +6,8 @@ public static class BssFrameworkSetupExtensions
 {
     public static TSelf AddNHibernate<TSelf>(
         this IBssFrameworkSetup<TSelf> settings,
-        Action<INHibernateSetup> setupAction) =>
+        Action<INHibernateSetup> setupAction)
+        where TSelf : IBssFrameworkSetup<TSelf> =>
 
         settings.AddExtensions(new BssFrameworkExtension(services => services.AddNHibernate(setupAction)));
 }

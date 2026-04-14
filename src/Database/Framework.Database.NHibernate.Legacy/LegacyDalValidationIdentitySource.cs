c443af4fad5c3ec6a76479478036acace3cf3423
iatsuta <terraghost@mail.ru>
2026-04-14 14:51:00 +0200

    clean

--- src/Database/Framework.Database.NHibernate.Legacy/LegacyDalValidationIdentitySource.cs
+++ src/Database/Framework.Database.NHibernate.Legacy/LegacyDalValidationIdentitySource.cs
@@ -1,13 +1,12 @@
 ﻿using System.Reflection;
 
-using Framework.ExtendedMetadata;
 using Framework.Validation;
 
 namespace Framework.Database.NHibernate;
 
-public class LegacyDalValidationIdentitySource(IMetadataProxyProvider metadataProxyProvider) : IDalValidationIdentitySource
+public class LegacyDalValidationIdentitySource : IDalValidationIdentitySource
 {
-    public string GetTypeValidationName(Type type) => metadataProxyProvider.Wrap(type).GetValidationName();
+    public string GetTypeValidationName(Type type) => type.GetValidationName();
 
     public string GetPropertyValidationName(PropertyInfo property) => property.GetValidationName();
 }

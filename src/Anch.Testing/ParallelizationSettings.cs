31772b4ccba1fffdbb981e3ef76904173f598951
Atsuta, Ivan <IAtsuta@luxoft.com>
2026-05-22 16:36:58 +0200

    clean

--- src/Anch.Testing/ParallelizationSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace Anch.Testing;
-
-public class ParallelizationSettings([FromKeyedServices] IEnumerable<AllowParallelizationConstraint> constraints) : IParallelizationSettings
-{
-    public bool AllowParallelization { get; } = constraints.All(c => c.Value);
-}

56c5c31a0ca8e1c50c0ffa9209e70c2ba1e636b4
iatsuta <98311820+iatsuta@users.noreply.github.com>
2026-06-14 23:30:46 +0200

    reg-security-bll-to-di (#646)

---
+++ src/BLL/Framework.BLL.Runtime/DependencyInjection/BllServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Anch.DependencyInjection;
+using Anch.SecuritySystem;
+
+using Framework.Application.Domain;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Framework.BLL.DependencyInjection;
+
+public static class BllServiceCollectionExtensions
+{
+    public static IServiceCollection AddBLL<TPersistentDomainObjectBase, TDomainObject, TIdent, TBllFactory, TBllFactoryImplement, TBll>(
+        this IServiceCollection services)
+        where TBllFactory : class, ISecurityBLLFactory<IDefaultSecurityDomainBLLBase<TPersistentDomainObjectBase, TDomainObject, TIdent>, TDomainObject>,
+        ISecurityBLLFactory<TBll>
+        where TBllFactoryImplement : class, TBllFactory
+        where TBll : class
+        where TDomainObject : class, TPersistentDomainObjectBase
+        where TPersistentDomainObjectBase : class, IIdentityObject<TIdent> =>
+        services.AddScoped<TBllFactory, TBllFactoryImplement>()
+                .AddScopedFrom<ISecurityBLLFactory<IDefaultSecurityDomainBLLBase<TPersistentDomainObjectBase, TDomainObject, TIdent>, TDomainObject>,
+                    TBllFactory>()
+                .AddKeyedScoped<TBll>(
+                    nameof(SecurityRule.Disabled),
+                    (sp, _) => ((ISecurityBLLFactory<TBll>)sp.GetRequiredService<TBllFactory>()).Create(SecurityRule.Disabled))
+                .AddKeyedScoped<TBll>(
+                    nameof(SecurityRule.View),
+                    (sp, _) => ((ISecurityBLLFactory<TBll>)sp.GetRequiredService<TBllFactory>()).Create(SecurityRule.View))
+                .AddKeyedScoped<TBll>(
+                    nameof(SecurityRule.Edit),
+                    (sp, _) => ((ISecurityBLLFactory<TBll>)sp.GetRequiredService<TBllFactory>()).Create(SecurityRule.Edit));
+}
--- src/_SampleSystem/SampleSystem.BLL/_Query/GetManagementUnitFluentMappings/GetManagementUnitFluentMappingsHandler.cs
+++ src/_SampleSystem/SampleSystem.BLL/_Query/GetManagementUnitFluentMappings/GetManagementUnitFluentMappingsHandler.cs
@@ -1,22 +1,21 @@
 ﻿using Anch.SecuritySystem;
+using Anch.SecuritySystem.Attributes;
 
 using MediatR;
 
 namespace SampleSystem.BLL._Query.GetManagementUnitFluentMappings;
 
-public class GetManagementUnitFluentMappingsHandler(IManagementUnitFluentMappingBLLFactory managementUnitFluentMappingBllFactory)
+public class GetManagementUnitFluentMappingsHandler([ViewSecurity] IManagementUnitFluentMappingBLL managementUnitFluentMappingBll)
     : IRequestHandler<GetManagementUnitFluentMappingsQuery,
         GetManagementUnitFluentMappingsResponse[]>
 {
-    private readonly IManagementUnitFluentMappingBLL managementUnitFluentMappingBll = managementUnitFluentMappingBllFactory.Create(SecurityRule.View);
-
     public async Task<GetManagementUnitFluentMappingsResponse[]> Handle(
             GetManagementUnitFluentMappingsQuery request,
             CancellationToken ct)
     {
-        var result = this.managementUnitFluentMappingBll.GetUnsecureQueryable()
-                         .Select(x => new GetManagementUnitFluentMappingsResponse(x.Id, x.Name, x.Parent.Id, x.Period))
-                         .ToArray();
+        var result = managementUnitFluentMappingBll.GetUnsecureQueryable()
+                                                   .Select(x => new GetManagementUnitFluentMappingsResponse(x.Id, x.Name, x.Parent.Id, x.Period))
+                                                   .ToArray();
 
         return result;
     }

3e14813bebb15551f65356721016ab53171e25f1
Atsuta, Ivan <IAtsuta@luxoft.com>
2026-06-30 20:53:51 +0200

    inc ver

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,7 +4,7 @@
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2009-2026")]
 
-[assembly: AssemblyVersion("27.3.7.0")]
+[assembly: AssemblyVersion("27.4.0.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]

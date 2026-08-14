ab56d772b61479c989cf85ffde95f9bd49dd8a98
Atsuta, Ivan <IAtsuta@luxoft.com>
2026-08-14 13:08:18 +0200

    add ef maps

---
+++ src/_Authorization/Framework.Authorization.Generated.DAL.EntityFramework/Mapping/SecurityContextTypeMap.cs
@@ -0,0 +1,16 @@
+﻿using Framework.Authorization.Domain;
+using Framework.Authorization.Generated.DAL.NHibernate.Mapping.Base;
+
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Framework.Authorization.Generated.DAL.NHibernate.Mapping;
+
+public class SecurityContextTypeMap : AuthBaseMap<SecurityContextType>
+{
+    public override void Configure(EntityTypeBuilder<SecurityContextType> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(x => x.Name).IsRequired();
+    }
+}

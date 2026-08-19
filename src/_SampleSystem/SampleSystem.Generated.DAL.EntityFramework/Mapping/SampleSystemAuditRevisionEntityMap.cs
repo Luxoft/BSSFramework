d872f687f755df7845cde0e4d6f15d4da3ba9896
Atsuta, Ivan <IAtsuta@luxoft.com>
2026-08-19 12:48:31 +0200

    add ss ef custom audit

---
+++ src/_SampleSystem/SampleSystem.Generated.DAL.EntityFramework/Mapping/SampleSystemAuditRevisionEntityMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using SampleSystem.AuditDomain;
+
+namespace SampleSystem.Generated.DAL.EntityFramework.Mapping;
+
+public class SampleSystemAuditRevisionEntityMap : IEntityTypeConfiguration<SampleSystemAuditRevisionEntity>
+{
+    public void Configure(EntityTypeBuilder<SampleSystemAuditRevisionEntity> builder)
+    {
+        builder.ToTable("AuditRevisionEntity", "appAudit");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasColumnName("id").ValueGeneratedOnAdd().IsRequired();
+        builder.Property(x => x.Author).HasColumnName("author").IsRequired();
+        builder.Property(x => x.RevisionDate).HasColumnName("RevisionDate").IsRequired();
+    }
+}

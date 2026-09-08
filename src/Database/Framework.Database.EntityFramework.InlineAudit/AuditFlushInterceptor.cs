7d2a17400f23ec2c25f794f5da66d63235159a86
Atsuta, Ivan <IAtsuta@luxoft.com>
2026-09-08 17:09:28 +0200

    start

--- src/Database/Framework.Database.EntityFramework.InlineAudit/AuditFlushInterceptor.cs
+++ src/Database/Framework.Database.EntityFramework.InlineAudit/AuditFlushInterceptor.cs
@@ -17,4 +17,11 @@ public class AuditFlushInterceptor(IEnumerable<InlineAuditBinding> inlineAuditBi
 
         return base.SavingChanges(eventData, result);
     }
+
+    public override ValueTask<int> SavedChangesAsync(
+        SaveChangesCompletedEventData eventData,
+        int result,
+        CancellationToken cancellationToken = new CancellationToken()) => base.SavedChangesAsync(eventData, result, cancellationToken);
+
+    public override int SavedChanges(SaveChangesCompletedEventData eventData, int result) => base.SavedChanges(eventData, result);
 }

efe5751de5639224bd162537d9fd1b892dcb3b83
Atsuta, Ivan <IAtsuta@luxoft.com>
2026-09-15 15:03:08 +0200

    add EnversTests

---
+++ src/_SampleSystem/_Tests/SampleSystem.IntegrationTests/EnversTests.cs
@@ -0,0 +1,12 @@
+﻿using Anch.Testing.Xunit;
+using SampleSystem.IntegrationTests._Environment.TestData;
+
+namespace SampleSystem.IntegrationTests;
+
+public abstract class EnversTests(IServiceProvider rootServiceProvider) : TestBase(rootServiceProvider)
+{
+    [AnchFact]
+    public async Task Test1(CancellationToken ct)
+    {
+    }
+}
